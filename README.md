9dd72ae2f934eaefc0225126d504e7fc228c6d26
ramon6767 <ramon.justis.ortega@gmail.com>
2023-01-31 11:37:06 -0800

    adds description and pictures and instructions. removes sam endpoint to clean up

---
+++ README.md
@@ -0,0 +1,8 @@
+Clone this repository, and run nodemon index.js from the terminal to view the Pokedex App!
+This App uses Axios http client to GET API JSON data from pokeapi.co.
+The data gets put into a Railway postGRESQLonline database and also is displayed on the screen.
+Search pokemon by image.
+See below!
+
+![alt text]("http://gitub.com/RamonJOrtega/pokemonApp1.png")
+![alt text]("http://gitub.com/RamonJOrtega/pokemonApp2.png")
--- index.js
+++ index.js
@@ -1,68 +1,94 @@
+const db = require('./database.js')
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const axios = require('axios');
 const { name } = require('ejs');
-
 app.use(express.static(__dirname + '/public/css'));
-
 app.use(expressLayouts);
 app.set('layout', './layouts/full-width');
 app.set('view engine', 'ejs');
 
 const port = 8001;
 
-// app.get('/', async (request, response) => {
-//     const apiResponse = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=5');
-//     const json = await apiResponse;
-//     console.log(json);
-//     response.render('helloWorld', {title: 'Home', text: 'This is an express app!'});
-// });
-namesAndPicsArray = [["n1,", "p1"]]; 
+const axiosGet = async (url) => {
+    console.log('The URL call is \n', url);
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    }
+        catch (err) { 
+            console.log("Axios Error: " + err);     
+        }
+}
 
 app.get('/', async (request, response) => {
-        const generalURL = 'https://pokeapi.co/api/v2/pokemon?limit=9';
-        const apiResponse = await axios.get(generalURL);
-        const namesAndURLs = apiResponse.data.results;
-        console.log(namesAndURLs[0].name);
-        //namesAndPicsArray = [["n1,", "p1"]]; //fill up a 2D array.
-        //each row is 1 name and 1 picture
-        for(let index = 0; index<namesAndURLs.length; index++){
-            let name = namesAndURLs[index]?.name;
-            let specificURL = namesAndURLs[index]?.url;
-            console.log(namesAndURLs[index]?.url);
-            let apiResponse = await axios.get(specificURL);
-            let pokePic = apiResponse.data.sprites.front_shiny;
-            namesAndPicsArray[index]=[name, pokePic];
-        }
-        console.log(namesAndURLs);
-        response.render('pokemonHome', {
-            title: 'Scroll down or select Pokemon Number from Above',
-            pokemon: namesAndPicsArray
-            });
+    const pokeCount = 150;
+    const generalURL = `https://pokeapi.co/api/v2/pokemon?limit=${pokeCount}`;
+    const apiResponse = await axiosGet(generalURL);
+    const namesAndURLs = apiResponse.results;
+    let namesAndPicsArray = []; //fill up a 2D array. //each row is 1 name and 1 picture
+    for(let index = 0; index<namesAndURLs.length; index++){
+        let name = namesAndURLs[index]?.name;
+        let specificURL = namesAndURLs[index]?.url;
+        console.log(namesAndURLs[index]?.url);
+        let apiResponse = await axiosGet(specificURL);
+        let pokePic = apiResponse.sprites.front_shiny;
+        namesAndPicsArray[index]=[name, pokePic];
+        // let inputRecordCommand = 
+        // `INSERT INTO pokemon (name, imageurl) 
+        // VALUES ('${name}', '${pokePic}')
+        // ON CONFLICT DO NOTHING;`
+        // db.giveMeString(inputRecordCommand)
+        // await db.executeQuery(inputRecordCommand).then(result => {
+        // if (result) {
+        //     console.log('query success');
+        // }
+        // })      
+    }
+    response.render('pokemonHome', {
+        title: 'Scroll down or select Pokemon Number from Above',
+        pokemon: namesAndPicsArray,
+        pokeCount: pokeCount
+        });
     });     
 
 app.get('/sam', (request, response) => {
     response.render('sam', {title: 'Sam' });
 });
 
-app.get('/number/:int', (request, response) => {
-    const myNumber = request.params.int;
-    response.render('number', {title: myNumber, number:myNumber},)
+app.get('/myPokemon', (request, response) => {
+    response.render('myPokemon', {title: 'My Pokemon' });
 });
+
+app.get('/number/:int', async (request, response) => {
+    const pokeNum = request.params.int;
+    const specificURL = `https://pokeapi.co/api/v2/pokemon/${pokeNum}`;
+    const apiResponsePoke = await axiosGet(specificURL);
+    const pokemonName = apiResponsePoke.forms[0].name;  
+    const imageURL = apiResponsePoke.sprites.front_shiny;
+    const height = apiResponsePoke.height;
+    const weight = apiResponsePoke.weight;
+    const speciesURL = apiResponsePoke.species.url;
+    const apiResponseSpecies = await axiosGet(speciesURL);
+    blurb = apiResponseSpecies.flavor_text_entries[0].flavor_text;
+    response.render('number', {
+    title: pokeNum, 
+    pokemon:pokemonName,
+    image:imageURL,
+    height: height,
+    weight: weight,
+    speciesURL: speciesURL,
+    blurb: blurb
+    });
+
+});
+
+const createBigTableCommand = 'CREATE TABLE IF NOT EXISTS pokemon (number serial PRIMARY KEY, name VARCHAR (255) UNIQUE NOT NULL, imageURL VARCHAR (255) UNIQUE NOT NULL, height VARCHAR (255) UNIQUE NOT NULL, weight VARCHAR (255) UNIQUE NOT NULL, speciesURL VARCHAR (255) UNIQUE NOT NULL, blurb VARCHAR (255) UNIQUE NOT NULL);'
+const createSmallTableCommand = 'CREATE TABLE IF NOT EXISTS pokemonNumberNameSprite (number serial PRIMARY KEY, name VARCHAR (255) UNIQUE NOT NULL, imageURL VARCHAR (255) UNIQUE NOT NULL);'
+const getDBrecordsCommand = 'Select * from pokemon'
+
 
 app.listen(port, () => {
     console.log(`listening to the smooth sounds of port ${port}`);
-});
-
-// if (typeof Window !== "undefined") {
-//     Window.onscroll = function() {myFunction()};
-// }
-
-// function myFunction() {
-//     if (document.documentElement.scrollTop > 10) {
-//       document.getElementById("myP").className = "test";
-//     } else {
-//       document.getElementById("myP").className = "";
-//     }
-//   }
+})
